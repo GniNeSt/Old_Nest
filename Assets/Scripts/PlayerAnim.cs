5ab9e22e8fcdecbec97917db575665e63b14f2c2
Junhyung-Choi <junhyung9571@naver.com>
2021-04-08 10:36:13 +0900

    add PlayerAnim.cs

---
+++ Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello hyojun");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
