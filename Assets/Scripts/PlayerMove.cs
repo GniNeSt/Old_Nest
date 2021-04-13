e214ea0743062458d6f7100ecacc93cd98a26cad
Junhyung-Choi <junhyung9571@naver.com>
2021-04-13 11:14:24 +0900

    modify choice,playermove editing groundmove

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    public GameObject choice_first;
+    public GameObject choice_second;
+    public GameObject choice_third;
+    public GameObject choice_fourth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
